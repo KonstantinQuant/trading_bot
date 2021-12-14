4853481737e4521d03549a1aa40bd9f661aca53c
Konstantin Kuchenmeister <88515336+KonstantinQuant@users.noreply.github.com>
2021-12-14 01:23:24 +0100

    Update tradingbot.py

--- tradingbot.py
+++ tradingbot.py
@@ -23,16 +23,23 @@ import matplotlib.pyplot as plt
 
 def getData():
   '''
-  retrieve the closing values of the stock
+  getData retrieves the closing values of the security that is used for training
+  and prediciton
+  ''' 
+  stock = pd.read_csv('BP.csv') # loading the dataset
+  stock = stock.dropna() # dropping all possible NA values
 
-  '''
-  msft = pd.read_csv('BP.csv')
-  msft = msft.dropna()
-  print(msft.head())
-  return msft.iloc[:, 5].values.reshape(-1,1)
+  # EDA
+  print(stock.head)
+  print(stock.describe())  
+  return stock.iloc[:, 5].values.reshape(-1,1) # return all adjusted closing prices
 
-print(getData().reshape(-1,1).shape)
-plt.plot(getData()[7000:])
+bp = getData()
+print("Shape:", bp.shape)
+
+# We drop the first 10000 values since it is too easy to make money and does not
+# reflect current volatilities
+plt.plot(bp[10_000:])
 
 """# 1. Create the Environment 
  
@@ -42,90 +49,129 @@ The environment represents all the "world" where the agent resides and stores al
 class TradingEnvironment:
 
   def __init__(self, data, initial_money):
-    self.stock_prices = data
+    self.stock_prices = data # the time series data of the security
 
-    self.initial_money = initial_money
-    self.shares_owned = None
-    self.cash = None
-    self.stock_price = None
+    self.initial_money = initial_money # the initial amount of money the agent gets
+    self.shares_owned = None # the number of shares owned
+    self.cash = None # the amount of cash on top shares owned
+    self.stock_price = None # the current stock price at the current step
 
-    self.n_steps = data.shape[0]
-    self.curr_step = None
+    self.n_steps = data.shape[0] # the number of observations in the data set
+    self.curr_step = None # the current step
 
-    # Define 3 actions: Sell = 0, Hold = 1, Buy = 2
-    self.actions = [0,1,2]
+    self.actions = [0,1,2] # Define 3 actions: Sell = 0, Hold = 1, Buy = 2
 
     self.reset()
 
   def get_state(self):
-    state = np.zeros(3)
-    state[0] = float(self.shares_owned)
-    state[1] = float(self.stock_price)
-    state[2] = float(self.cash)
+    '''
+    get_state returns the current representaiton of the environment to the agent.
+    The state is stored in the form of a numpy array with:
+        1. number of shares in the portfolio at state s at time t
+        2. current stock price at which the shares are traded in state s at time t
+        3. amount of cash the agent has in the portfolio
+    '''
+    state = np.zeros(3) # intialize the state
+    state[0] = float(self.shares_owned) # the number of shares
+    state[1] = float(self.stock_price) # the current stock price
+    state[2] = float(self.cash) # the amount of cash
     return state
 
 
   def reset(self):
-    self.curr_step = 0
-    self.shares_owned = 0
-    self.stock_price = self.stock_prices[self.curr_step]
-    self.cash = self.initial_money
-    return self.get_state()
+    '''
+    reset resets the environment to an standardized initial starting state.
+    This will be done in the beginning of each episode durin the training phase.
+    The agent will be placed at the beginning of the time series, owning no
+    shares but having the amount of intial cash given.
+    This function will then give the agent the state of this first standard
+    environment.
+    '''
+    self.curr_step = 0 # first point in the time series
+    self.shares_owned = 0 # owning no shares
+    self.stock_price = self.stock_prices[self.curr_step] # stock price at time t = 0
+    self.cash = self.initial_money # agent will have the initial investment at t = 0
+    return self.get_state() # return the first state to the agent
     
 
   def trade(self, action):
+    '''
+    At every timestep during each episode the agent will perform an action.
+    He will trade, implying he will buy sell or hold.
+    If he decides to buy, he will buy shares until we run out of money.
+    If he decides to sell, he will sell all our shares giving us the money back.
+    If he decides to hold, he will earn the risk free interest rate.
+    '''
 
     # sell all shares
     if action == 0:
+      # we will get the current value of the shares back to our cash account
       self.cash = self.cash + self.stock_price * self.shares_owned
+      # selling all shares
       self.shares_owned = 0
-      #print("I sold all shares.")
 
     # buy shares until we run out of money
     if action == 2:
       liquid = True
 
+      # as long as we have cash, buy a share
       while liquid:
         if self.cash >= self.stock_price:
-          self.shares_owned = self.shares_owned + 1
-          self.cash = self.cash - self.stock_price # buy
+          self.shares_owned = self.shares_owned + 1 # buy a share
+          self.cash = self.cash - self.stock_price # deduct the money
           #print("I bought a share for:", self.stock_price, "we own", self.shares_owned, "shares.")
         else:
           liquid = False
 
       if action == 1:
-        self.cash = self.cash * 1+(0.01/252) # for each day the agent chooses to not do anything, he earns the risk free interest rate
+         # for each trading day the agent chooses to not do anything, he earns the risk free interest rate
+        self.cash = self.cash * 1+(0.01/252)
 
   def _get_balance(self):
+    ''' 
+    _get_balance will return the current balance which is the number of shares 
+    we own times the stock price plus the cash we have
+    '''
     return self.shares_owned * self.stock_price + self.cash
 
   
   def make_step(self, action):
+    '''
+    At every time step of the time series, make a step. The agent retrieves a new
+    representation of the state, makes a step, receives the reward with the new state.
+    '''
+    balance = self._get_balance() # the balance before the step
 
-    balance = self._get_balance()
+    self.curr_step = self.curr_step + 1 # making a step
 
-    self.curr_step = self.curr_step + 1
+    self.stock_price = self.stock_prices[self.curr_step] # updating the stock price
 
-    self.stock_price = self.stock_prices[self.curr_step]
+    self.trade(action) # performing the trade
 
-    self.trade(action)
+    newbalance = self._get_balance() # storing the new balance
 
-    newbalance = self._get_balance()
+    reward = newbalance - balance # the reward will be the profit of the trade
 
-    reward = newbalance - balance
-
-    done = self.curr_step == self.n_steps-1
-
+    # an episode can end based on two conditions:
+    # either we are at the last step
+    # or we ran out of money, in this case if the trades are so bad we lost 95%
+    #done = False # indicates whether the current episode is done
+    #if self.curr_step == self.n_steps-1:
+     # done = True
+    #elif newbalance <= self.initial_money * .05:
+     # done = True
+    #else:
+     # done = False
+    done = self.curr_step == self.n_steps-1 # if we are at the last 
+    print(done)
+    # verbosity
     print("Step:", self.curr_step)
     print("Old Balance", balance, " New Balance:", newbalance, "Reward:", reward)
 
+    # returning the info to the agent
     return self.get_state(), reward, done, newbalance
 
-"""# 2. Create the Replay Buffer
-
-We will probably just use a deque
-
-# 3. Create the Agent 
+"""# 2. Create the Agent 
 
 (with deep neural network)
 """
@@ -273,10 +319,12 @@ state_dim = 1*2+1 # tbd
 gordongecko = TradingAgent(state_dim)
 
 # Adjust these as wished
-EPISODES = 500 
+EPISODES = 1 
 
 for e in range(EPISODES):
   print("Episode:",e)
   portfolio = list()
   val = play_episode(gordongecko, env, True, scaler)
   portfolio.append(val)
+  plt.plot(portfolio)
+
